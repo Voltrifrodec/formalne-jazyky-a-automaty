c3e3c3f5c6e0c9ba76ef06aaa7e2276658f073b6
Voltrifrodec <nendorans@gmail.com>
2024-12-02 01:16:12 +0100

    [feature]: Add documentation and code cleanup

--- content/zadanie-2/aplikacia/src/main/java/sk/umb/fpv/fja/zadanie_2/CalculatorException.java
+++ content/zadanie-2/aplikacia/src/main/java/sk/umb/fpv/fja/zadanie_2/CalculatorException.java
@@ -1,13 +1,13 @@
 package sk.umb.fpv.fja.zadanie_2;
 
 public class CalculatorException extends RuntimeException {
-    
-    public CalculatorException(String message) {
-        super(message);
-    }
 
-    public CalculatorException(String message, Throwable cause) {
-        super(message, cause);
-    }
+	public CalculatorException(String message) {
+		super(message);
+	}
+	
+	public CalculatorException(String message, Throwable cause) {
+		super(message, cause);
+	}
 
 }
